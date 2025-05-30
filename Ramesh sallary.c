b6930e918be9767b36a2d1f345bb53020cafaa22
Atharvkalekar15 <atharvkalekar15@gmail.com>
2025-05-30 14:12:38 +0530

    Ramesh sallary.c

---
+++ Ramesh sallary.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    float basic, da, hra, gross;
+    printf("Enter Ramesh's basic salary: ");
+    scanf("%f", &basic);
+
+    da = 0.4 * basic;
+    hra = 0.2 * basic;
+    gross = basic + da + hra;
+
+    printf("Gross Salary: %.2f\n", gross);
+
+    return 0;
+}
